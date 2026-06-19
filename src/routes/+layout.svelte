95a220bf59ac68860704f1ee702329df1bd41ca6
Bob McAleavey <bobmcaleavey@gmail.com>
2026-06-19 07:46:39 -0400

    feat: add PWA components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,4 +36,19 @@
 	});
 </script>
 
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href="/icons/icon.svg" />
+	<link rel="alternate icon" href="/icons/icon-16x16.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-apple-touch.png" />
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="theme-color" content="#FF0040" />
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, user-scalable=no, viewport-fit=cover"
+	/>
+	<title>ABTL - Fire Show - 2026</title>
+</svelte:head>
+
 {@render children()}
